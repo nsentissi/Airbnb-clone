80f3ebe17a63fded83be48e322a8493e6e9b5978
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-02-20 13:02:42 +0100

    created login and register page, and an express server with a basic route for testing

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require ('express')
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config()
+const app= express();
+
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+}));
+
+
+mongoose.connect(process.env.MONGO_URL)
+
+app.get('/test', (req,res) => {
+    res.json('test ok')
+})
+
+app.post('/register', (req,res) => {
+    const {name, email, password} =req.body
+    res.json({name, email, password})
+})
+
+app.listen(3000)
